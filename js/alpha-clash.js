f101a7852753793bc8d3ea04fd515fe88b2a8e8b
rontysarkar <rontysarkar2023@gmail.com>
2024-02-15 16:18:07 +0600

    add all section

--- js/alpha-clash.js
+++ js/alpha-clash.js
@@ -1,22 +1,35 @@
 function keyEvent(event) {
-  const playerPressed = event.key;
+  const playerPressed = event.key.toLowerCase();
+  if (playerPressed === "escape") {
+    gameOver();
+  }
+  console.log(playerPressed);
+
   const currentAlphabet = document.getElementById("current-alphabet");
   const expected = currentAlphabet.innerText.toLowerCase();
-  if(expected === playerPressed){
-    console.log('you get a point');
-  }
-  else{
-    console.log('losss life')
+  if (expected === playerPressed) {
+    const value = getTextElementById("current-score");
+    const newScore = value + 1;
+    setTextElementById("current-score", newScore);
+
+    removeBackgroundColor(playerPressed);
+    contunueGame();
+  } else {
+    const value = getTextElementById("current-life");
+    const newLife = value - 1;
+    setTextElementById("current-life", newLife);
+    if (newLife === 0) {
+      gameOver();
+    }
   }
 }
 
 document.addEventListener("keyup", keyEvent);
 
-// contune game
+//  game play
 
 function contunueGame() {
   const alphabet = getAlphaBet();
-  console.log("Random Alphabet :", alphabet);
 
   const currentAlphabet = document.getElementById("current-alphabet");
   currentAlphabet.innerText = alphabet;
@@ -26,6 +39,21 @@ function contunueGame() {
 
 function play() {
   hiddenElement("home-section");
+  hiddenElement("final-score");
   showElement("playground-section");
+
+  setTextElementById("current-life", 5);
+  setTextElementById("current-score", 0);
+
   contunueGame();
+}
+
+function gameOver() {
+  hiddenElement("playground-section");
+  showElement("final-score");
+  getElementTextById("current-alphabet");
+
+  const lastScoreElement = document.getElementById("current-score");
+  const lastScore = lastScoreElement.innerText;
+  setTextElementById("last-score", lastScore);
 }
--- js/utility.js
+++ js/utility.js
@@ -13,6 +13,29 @@ function setBackgrundColor(elementId) {
   element.classList.add("bg-orange-400");
 }
 
+function removeBackgroundColor(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove("bg-orange-400");
+}
+
+function getTextElementById(elementId) {
+  const element = document.getElementById(elementId);
+  const elementText = element.innerText;
+  const value = parseInt(elementText);
+  return value;
+}
+
+function setTextElementById(elementId, value) {
+  const element = document.getElementById(elementId);
+  element.innerText = value;
+}
+
+function getElementTextById(elementId) {
+  const currentAlphabet = document.getElementById(elementId);
+  const currentText = currentAlphabet.innerText;
+  removeBackgroundColor(currentText);
+}
+
 function getAlphaBet() {
   const alphaBetString = "abcdefghijklmnopqrsyuvwxyz";
   const alphaBet = alphaBetString.split("");
